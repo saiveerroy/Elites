b1ccfb7764ed6b67f416c68635480ecaa9be3954
saiveerroy <saiveer331@gmail.com>
2026-03-23 13:59:14 +0530

    changed

--- frontend-react/src/pages/Category.jsx
+++
@@ -1,9 +0,0 @@
-import { useParams } from "react-router-dom";
-
-function Category() {
-  const { name } = useParams();
-
-  return <h1>{name.toUpperCase()} Collection</h1>;
-}
-
-export default Category;
